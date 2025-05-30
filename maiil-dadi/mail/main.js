8869b3704095d6c475a6463c6bf7581f9beab3bc
nicoladomingorizzo <nicoladomingorizzo@gmail.com>
2025-05-30 14:32:41 +0200

    Add initial HTML and JavaScript files for dice game and email verification features; update .gitignore

---
+++ maiil-dadi/mail/main.js
@@ -0,0 +1,20 @@
+/* Mail
+Crea una lista di email di invitati ad una festa. Chiedi all’utente la sua email, controlla che sia nella lista di chi può accedere, stampa un messaggio appropriato sull’esito del controllo, utilizzando un ciclo for. Non è consentito usare nessun metodo proprio degli array (come includes, per esempio). Si può fare? Certo che si basta ragionare un po’. Nota: Non è necessario provvedere alla validazione delle email */
+
+// Crea una lista di email di invitati ad una festa
+let mailList = []; // array creata
+// Chiedi all’utente la sua email
+const userMail = prompt('Inserisci la tua mail per poter partecipare alla festa'); //promptmail creata
+let userMailVerified = false; // let userMailVerified creata 
+mailList.push(userMail); // promptmail inserito nella mail list
+// controlla che sia nella lista di chi può accedere
+for (let i = 0; i < mailList.length; i++) {
+    console.log(mailList[i])
+    if (mailList == userMail) {
+        let userMailVerified = true
+        break
+    }
+}; // ciclo for sostitutivo a .includes
+// stampa un messaggio appropriato sull’esito del controllo, utilizzando un ciclo for
+console.log("La mail è verficata, l'utente può accedere alla festa"); // log message inserito
+alert("La mail è verficata, puoi accedere alla festa"); // alert message inserito
