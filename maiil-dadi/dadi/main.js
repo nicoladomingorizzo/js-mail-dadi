094a2c29bde1716df843ae060855acc13f63e657
nicoladomingorizzo <nicoladomingorizzo@gmail.com>
2025-05-30 15:12:45 +0200

    insert ;

--- maiil-dadi/dadi/main.js
+++ maiil-dadi/dadi/main.js
@@ -7,23 +7,23 @@ let diceResult = []; // Array con i risultati random dei dadi
 // Generare un numero random da 1 a 6, sia per il giocatore sia per il computer.
 let userDieResult = Math.floor(Math.random() * 6 + 1); // genera il numero random dell'utente
 diceResult.push(userDieResult); // aggiungi il numero dell'utente all'array
-alert("Il dado lanciato dall'utente ha dato il numero : " + userDieResult)
+alert("Il dado lanciato dall'utente ha dato il numero : " + userDieResult);
 let computerDieResult = Math.floor(Math.random() * 6 + 1); // genera il numero del computer
 diceResult.push(computerDieResult); // aggiungi il numero random del computer all'array
-alert('Il dado lanciato dal computer ha dato il numero : ' + computerDieResult)
+alert('Il dado lanciato dal computer ha dato il numero : ' + computerDieResult);
 console.log(diceResult); // log risultati numeri random
 
 // Stabilire il vincitore, in base a chi fa il punteggio più alto.
 if (userDieResult > computerDieResult) {
-    console.log("Vince l'utente perché il numero uscito dal lancio del dado è più alto")
-    alert("Vince l'utente perché il numero uscito dal lancio del dado è più alto")
+    console.log("Vince l'utente perché il numero uscito dal lancio del dado è più alto");
+    alert("Vince l'utente perché il numero uscito dal lancio del dado è più alto");
 } else if (userDieResult < computerDieResult) {
-    console.log("Vince il computer perché il numero uscito dal lancio del dado è più alto")
-    alert("Vince il computer perché il numero uscito dal lancio del dado è più alto")
+    console.log("Vince il computer perché il numero uscito dal lancio del dado è più alto");
+    alert("Vince il computer perché il numero uscito dal lancio del dado è più alto");
 } else {
     //if (userDieResult == computerDieResult) {
-    console.log("I numeri usciti dal lancio dei dadi sono uguali, rilancia i dadi")
-    alert("I numeri usciti dal lancio dei dadi sono uguali, rilancia i dadi")
+    console.log("I numeri usciti dal lancio dei dadi sono uguali, rilancia i dadi");
+    alert("I numeri usciti dal lancio dei dadi sono uguali, rilancia i dadi");
     // userDieResult = Math.floor(Math.random() * 6 + 1); // genera il numero random dell'utente
     // computerDieResult = Math.floor(Math.random() * 6 + 1); // genera il numero del computer
     // }
