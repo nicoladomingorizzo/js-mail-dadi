101b4fa22948023864d0c77f639f30d20852b4c1
nicoladomingorizzo <nicoladomingorizzo@gmail.com>
2025-05-30 15:36:47 +0200

    Add bonus js loop while

---
+++ bonus/prova.js
@@ -0,0 +1,26 @@
+/* Gioco dei dadi
+Generare un numero random da 1 a 6, sia per il giocatore sia per il computer. Stabilire il vincitore, in base a chi fa il punteggio più alto.
+Prima di partire a scrivere codice poniamoci qualche domanda: Che ci sia un array da qualche parte? Se dobbiamo confrontare qualcosa che "cosa" ci serve? */
+
+let flag = false // flag per ciclo while
+
+while (flag == false) {
+    // Generare un numero random da 1 a 6, sia per il giocatore sia per il computer.
+    let userDieResult = Math.floor(Math.random() * 6 + 1); // genera il numero random dell'utente
+    alert("Il dado lanciato dall'utente ha dato il numero : " + userDieResult);
+    let computerDieResult = Math.floor(Math.random() * 6 + 1); // genera il numero del computer
+    alert('Il dado lanciato dal computer ha dato il numero : ' + computerDieResult);
+    // Stabilire il vincitore, in base a chi fa il punteggio più alto.
+    if (userDieResult > computerDieResult) {
+        console.log("Vince l'utente perché il numero uscito dal lancio del dado è più alto");
+        alert("Vince l'utente perché il numero uscito dal lancio del dado è più alto");
+        flag = true;
+    } else if (userDieResult < computerDieResult) {
+        console.log("Vince il computer perché il numero uscito dal lancio del dado è più alto");
+        alert("Vince il computer perché il numero uscito dal lancio del dado è più alto");
+        flag = true;
+    } else {
+        console.log("I numeri usciti dal lancio dei dadi sono uguali, rilancia i dadi");
+        alert("I numeri usciti dal lancio dei dadi sono uguali, rilancia i dadi");
+    }
+}
